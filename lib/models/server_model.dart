ebb9c3f664a75e0b5cb3228f7d874dc74a835d1e
Shaheryar Ashfaq <96656807+shaheryar003@users.noreply.github.com>
2022-07-09 15:26:38 +0500

    Add files via upload

--- lib/models/server_model.dart
+++ lib/models/server_model.dart
@@ -12,39 +12,11 @@ class Flag {
 }
 
 List<Flag> flaglist = [
+
   Flag(
-    id: 1,
-    image: 'assets/country/flag/ic_vietnamese.png',
-    title: "Vietnam",
-  ),
-  Flag(
-    id: 2,
-    image: "assets/country/flag/ic_us.png",
-    title: "United States",
-  ),
-  Flag(
-    id: 3,
-    image: "assets/country/flag/ic_ar.png",
-    title: "United Arab Emirates",
-  ),
-  Flag(
-    id: 4,
-    image: "assets/country/flag/ic_thai.png",
-    title: "Thailand",
-  ),
-  Flag(
-    id: 5,
-    image: "assets/country/flag/ic_india.png",
-    title: "India",
-  ),
-  Flag(
-    id: 6,
+    id:1 ,
     image: "assets/country/flag/ic_korean.png",
-    title: "Korea",
+    title: "South Korea",
   ),
-  Flag(
-    id: 7,
-    image: "assets/country/flag/ic_japanese.png",
-    title: "Japan",
-  ),
+
 ];
